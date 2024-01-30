277faf847480dd573033a8c1787c3d4f9db0d4bc
Aleksandr Paskal <sasha.paskal@yandex.ru>
2024-01-30 19:46:25 +0300

    переписал код на JS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	const currentYear = new Date().getFullYear(); // императивный
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-				<p>{currentYear} год</p>
-			</header>
-		</div>
-	);
+	const currentYear = new Date().getFullYear();
+	const appDiv = document.createElement('div');
+	appDiv.className = 'App';
+
+	const header = document.createElement('header');
+	header.className = 'App-header';
+
+	const image = document.createElement('img');
+	image.src = logo;
+	image.className = 'App-logo';
+	image.alt = 'logo';
+
+	const paragraph = document.createElement('p');
+	const code = document.createElement('code');
+	code.textContent = 'src/App.js';
+	paragraph.appendChild(document.createTextNode('Edit '));
+	paragraph.appendChild(code);
+	paragraph.appendChild(document.createTextNode(' and save to reload.'));
+
+	const link = document.createElement('a');
+	link.className = 'App-link';
+	link.href = 'https://reactjs.org';
+	link.target = '_blank';
+	link.rel = 'noopener noreferrer';
+	link.textContent = 'Learn React';
+
+	const yearParagraph = document.createElement('p');
+	yearParagraph.textContent = currentYear;
+
+	header.appendChild(image);
+	header.appendChild(paragraph);
+	header.appendChild(link);
+	header.appendChild(yearParagraph);
+	appDiv.appendChild(header);
+
+	return appDiv;
 };
